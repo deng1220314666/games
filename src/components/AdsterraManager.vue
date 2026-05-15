5c61790f7b6cb6243b37428448545d06db98891f
Deng <1220314666@qq.com>
2026-05-15 23:14:07 +0800

    增加埋点

--- src/components/AdsterraManager.vue
+++ src/components/AdsterraManager.vue
@@ -110,23 +110,23 @@ const showAd = (type) => {
   if (props.immediate) {
     AdsterraAd.showBannerImmediate(props.idTxt, options, script);
   } else {
-    // const options = {
-    //   key: 'd485bca4ce91450e3b58525457ee556a',
-    //   format: 'iframe',
-    //   height: 250,
-    //   width: 300,
-    //   params: {}
-    // }
-    // AdsterraAd.showBanner(props.idTxt, options, "https://freshmanhow.com/d485bca4ce91450e3b58525457ee556a/invoke.js");
-
     const options = {
-      'key' : '8dcf53405071d2838182738cbc9a4c0e',
-      'format' : 'iframe',
-      'height' : 250,
-      'width' : 300,
-      'params' : {}
+      key: 'd485bca4ce91450e3b58525457ee556a',
+      format: 'iframe',
+      height: 250,
+      width: 300,
+      params: {}
     }
-    AdsterraAd.showBanner(props.idTxt, options, "https://freshmanhow.com/8dcf53405071d2838182738cbc9a4c0e/invoke.js");
+    AdsterraAd.showBanner(props.idTxt, options, "https://freshmanhow.com/d485bca4ce91450e3b58525457ee556a/invoke.js");
+
+    // const options = {
+    //   'key' : '8dcf53405071d2838182738cbc9a4c0e',
+    //   'format' : 'iframe',
+    //   'height' : 250,
+    //   'width' : 300,
+    //   'params' : {}
+    // }
+    // AdsterraAd.showBanner(props.idTxt, options, "https://freshmanhow.com/8dcf53405071d2838182738cbc9a4c0e/invoke.js");
 
     gaLogEvent.logEvent({
       eventName: "adsterra_banner_success",
