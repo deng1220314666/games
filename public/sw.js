11fd9e11a2e4c9305882e391672646094e1d90ea
Deng <1220314666@qq.com>
2026-05-18 10:32:11 +0800

    添加Monetag配置

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,13 @@
 /// <reference lib="webworker" />
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11021496
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
 // 缓存配置
 const CACHE_VERSION = 'v0.0.9';
 const CACHE_PREFIX = 'TTGame';
