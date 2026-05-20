fe106fd49bb233acfe3e2a72fa6a2c9992fdbc2a
Deng <denglianghui@minigame.vip>
2026-05-20 12:09:27 +0800

    首页广告调整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home w-full box-border max-w-7xl mx-auto px-2 sm:px-1 lg:px-2 py-2 flex-1 overflow-y-auto hide-scrollbar z-0 pb-16">
 <!--    <div id="onclick-video-1"></div>-->
-    <div data-banner-id="6119451" class="flex items-center justify-center mb-[1rem]"></div>
+<!--    <div data-banner-id="6119451" class="flex items-center justify-center mb-[1rem]"></div>-->
     <div v-for="(item, index) in category" :key="index" class="w-full mb-4">
 <!--      <AdsterraManager idTxt="adsterra-banner-2-box" :zid="2" v-if="index === 2" />-->
 <!--      <AdsterraManager idTxt="adsterra-banner-3-box" :zid="3" v-if="index === 3" />-->
@@ -93,7 +93,7 @@ onMounted(async () => {
   // MonetagAd.pushShow();
   requestNotifyPermission();
   // OnClickA.loadOnclickAd();
-  OnClickA.showBanner();
+  // OnClickA.showBanner();
   // loadOnclickScript("https://js.onclckmn.com/static/onclicka.js", "153207");
   // setTimeout(() => {
   //   pushRouterHistory();
